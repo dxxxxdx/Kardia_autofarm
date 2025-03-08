87041f22d4441bbad1a122fc3865e002e9d2485d
dxxxxdx <danshang@easecation.net>
2025-03-08 19:58:43 +0800

    11111

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import core.recongnize_process as rp
 import core.main_enter
 
 #基于opencv的图像识别，需要下载电脑模拟器并且将其置于屏幕才可以使用
+#tttt
 
 def main():
     rp.recongnize_process()
     core.main_enter.main_enter()
 
+
+    a = 1
 if __name__ == "__main__":
     main()
